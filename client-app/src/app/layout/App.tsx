d763dced4f15451f853881734e24321cf516bbc5
kuan.samuel <kuan.samuel@gmail.com>
2023-07-31 17:04:15 +0800

    NavBar Update

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Button, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(response => {
         setActivities(response.data);
       })
@@ -14,9 +16,10 @@ function App() {
 
   return (
     <div>
-      <Header as='h2' icon='users' content='Reactivities'/>
+      <NavBar />
+      
         <List>
-          {activities.map((activity: any) => (
+          {activities.map(activity => (
             <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
         </List>
